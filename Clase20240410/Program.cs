6d1137df676b7403f822ac078ed5bfa100963966
sergio_valle <sergiovallegarma@gmail.com>
2024-04-10 22:56:06 -0500

    clase 2024 04 10 tema: clases public private y static

---
+++ Clase20240410/Program.cs
@@ -0,0 +1,90 @@
+﻿
+using Clase20240410;
+using System.Drawing;
+
+class Program
+{
+    static void Main()
+    {
+        #region PrimeraForma
+        //Constructor por defecto
+        Punto punto1 = new Punto(0, 0);
+        Punto punto2 = new Punto(3, 0);
+        Punto punto3 = new Punto(0, 4);
+        Triangulo triangulo = new Triangulo();
+        triangulo.Punto1 = punto1;
+        triangulo.Punto2 = punto2;
+        triangulo.Punto3 = punto3;
+
+
+        Console.WriteLine("PRIMERA FORMA");
+        Console.WriteLine(triangulo.CalcularArea());
+        Console.WriteLine(triangulo.CalcularPerimetro());
+        #endregion
+
+
+        #region SegundForma
+
+        Punto punto1a = new Punto(0, 0);
+        Punto punto2a = new Punto(3, 0);
+        Punto punto3a = new Punto(0, 4);
+
+
+        Triangulo trianguloa = new Triangulo(punto1a, punto2a, punto3a);
+        Console.WriteLine("SEGUNDA FORMA");
+        Console.WriteLine(trianguloa.CalcularArea());
+        Console.WriteLine(trianguloa.CalcularPerimetro());
+        #endregion
+
+        #region TerceraForma
+        //Rectangulo rectangulob = new Rectangulo
+        Triangulo triangulob = new()
+        {
+            Punto1 = new Punto(0, 0),
+            Punto2 = new Punto(3, 0),
+            Punto3 = new Punto(0, 4)
+        };
+        Console.WriteLine("TERCERA FORMA");
+        Console.WriteLine(triangulob.CalcularArea());
+        Console.WriteLine(triangulob.CalcularPerimetro());
+
+        #endregion
+
+
+
+
+        Console.Read();
+
+
+
+    }
+
+    //static void ValorReferencia()
+    //{
+    //    // Ejemplo con estructura
+    //    PuntoStruct puntoStruct1 = new PuntoStruct();
+    //    puntoStruct1.X = 10;
+    //    puntoStruct1.Y = 20;
+
+    //    PuntoStruct puntoStruct2 = puntoStruct1; // Se copia por valor
+    //    puntoStruct2.X = 30;
+
+    //    Console.WriteLine("Estructura:");
+    //    Console.WriteLine("Punto 1: X = " + puntoStruct1.X + ", Y = " + puntoStruct1.Y);
+    //    Console.WriteLine("Punto 2: X = " + puntoStruct2.X + ", Y = " + puntoStruct2.Y);
+    //    Console.WriteLine();
+
+    //    // Ejemplo con clase
+    //    PuntoClase puntoClase1 = new PuntoClase();
+    //    puntoClase1.X = 10;
+    //    puntoClase1.Y = 20;
+
+    //    PuntoClase puntoClase2 = puntoClase1; // Se copia la referencia
+    //    puntoClase2.X = 30;
+
+    //    Console.WriteLine("Clase:");
+    //    Console.WriteLine("Punto 1: X = " + puntoClase1.X + ", Y = " + puntoClase1.Y);
+    //    Console.WriteLine("Punto 2: X = " + puntoClase2.X + ", Y = " + puntoClase2.Y);
+    //}
+}
+
---
+++ Clase20240410/Triangulo.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase20240410
+{
+    public class Triangulo
+    {
+        public Punto Punto1 { get; set; }
+        public Punto Punto2 { get; set; }
+        public Punto Punto3 { get; set; }
+
+        public Triangulo()
+        {
+
+        }
+
+        public Triangulo(Punto punto1, Punto punto2, Punto punto3)
+        {
+            Punto1 = punto1;
+            Punto2 = punto2;
+            Punto3 = punto3;
+
+        }
+
+
+
+
+        #region FuncionesPrivadas
+
+        //private double CalcularDistancia(double a1, double b1, double a2, double b2)
+        //{
+        //    double result;
+        //    result = Math.Sqrt(Math.Pow((a2 - a1), 2) + Math.Pow((b2 - b1), 2));
+        //    return result;
+
+        //}
+        //Helper helper = new Helper();
+        private double ObtenerAltura()
+        {
+            double result;
+            result = Helper.CalcularDistancia(Punto1.X, Punto1.Y, Punto2.X, Punto2.Y);
+            return result;
+        }
+        private double ObtenerBase()
+        {
+            double result;
+            result = Helper.CalcularDistancia(Punto1.X, Punto1.Y, Punto3.X, Punto3.Y);
+            return result;
+        }
+
+        private double ObtenerDiagonal()
+        {
+            double result;
+            result = Helper.CalcularDistancia(Punto2.X, Punto2.Y, Punto3.X, Punto3.Y);
+            return result;
+        }
+
+        #endregion
+
+        #region FuncionesPublicas
+
+        public double CalcularArea()
+        {
+            double result;
+            result = ObtenerBase() * ObtenerAltura()/2;
+            return result;
+        }
+
+        public double CalcularPerimetro()
+        {
+            double result;
+            result = ObtenerBase() + ObtenerAltura() + ObtenerDiagonal();
+            return result;
+        }
+        #endregion
+
+
+
+    
+
+
+}
+}
